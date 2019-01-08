c469b3bb2dece6f661f03ca1ad06b67c6fcd4dea
Byeongguk Gong <byeonggukgong@gmail.com>
2019-01-08 17:06:16 +0900

    Integrate Dynamoose

---
+++ src/middlewares/dynamoose.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import dynamoose from 'dynamoose';
+
+dotenv.config();
+
+dynamoose.AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+dynamoose.local();
+
+export default dynamoose;
